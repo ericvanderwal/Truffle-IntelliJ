9377d54daed009c9e60599e68a8e771d2048b5ea
GitHub Action <action@github.com>
2021-08-08 12:37:30 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/ericvanderwal/truffleintellij/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.ericvanderwal.truffleintellij.services
+
+import com.github.ericvanderwal.truffleintellij.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
